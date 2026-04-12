c9411b92f42b6c24d13a01d94cac1a883645955a
Mahbuba Bithi <mahbubabithi2393@gmail.com>
2026-04-12 23:44:32 +0000

    learned var, let, const

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,10 @@
+const accountID = 12345
+let accountEmail = "mqhduhi"
+var accountPassword = "12334"
+let accountCity = "habughs"
+
+accountEmail = "hc@hc.com"
+accountPassword = "123243"
+accountCity = "dhaka"
+
+console.table([accountID, accountEmail, accountPassword, accountCity])
